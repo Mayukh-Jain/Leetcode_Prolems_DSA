ebd9a8ed6aa24ebc0fa6a28c1b79329f472f566e
Mayukh Jain <jainmayukh@gmail.com>
2026-07-25 14:09:11 +0530

    Time: 0 ms (100%), Space: 19.2 MB (86.2%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -1,11 +1,13 @@
 class Solution:
     def numSteps(self, s: str) -> int:
-        n=int(s,2)
-        count=0
-        while n>1:
-            if n%2==0:
-                n=n//2
+        st=0
+        carry=0
+        for i in range(len(s)-1,0,-1):
+            val=int(s[i])+carry
+            if val==1:
+                st+=2
+                carry=1
             else:
-                n+=1
-            count+=1
-        return count
+                st+=1
+
+        return st+carry
