1afa8397b7a9aeda0d832d5f9ea9ab845a56591f
Mayukh Jain <jainmayukh@gmail.com>
2026-09-06 15:26:26 +0530

    Time: 213 ms (85.86%), Space: 19.4 MB (91.77%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,18 +1,27 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         n,m=len(s),len(t)
-        dp=[[0]*(m+1) for i in range(n+1)]
-        for i in range(n+1):
-            dp[i][0]=1
+        dp=[1]+[0]*m
 
-        for i in range(1,n+1):
-            for j in range(1,m+1):
-                if s[i-1]==t[j-1]:
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
-                else:
-                    dp[i][j]=dp[i-1][j]
+        for ch in s:
+            for j in range(m,0,-1):
+                if ch==t[j-1]:
+                    dp[j]+=dp[j-1]
+        return dp[m]
         
-        return dp[n][m]
+        # n,m=len(s),len(t)
+        # dp=[[0]*(m+1) for i in range(n+1)]
+        # for i in range(n+1):
+        #     dp[i][0]=1
+
+        # for i in range(1,n+1):
+        #     for j in range(1,m+1):
+        #         if s[i-1]==t[j-1]:
+        #             dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
+        #         else:
+        #             dp[i][j]=dp[i-1][j]
+        
+        # return dp[n][m]
 
         # def solve(i,j):
         #     if j==0: return 1
